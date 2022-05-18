9e11fcf8273f6eb61ebef9b44b67f7f3c1bc5d58
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-05-18 12:21:04 +0100

    Update renverse.py

--- renverse.py
+++ renverse.py
@@ -1,8 +1,17 @@
+#: The function to reverse a word
 def renverse(mot):
+	"""Translate the given text to Parseltongue.
+
+    :param str text: word to reverse.
+
+    :returns: the reversed word .
+    :rtype: str
+    """
 	tom,res,l,mot = [],"",len(mot),list(mot)
 	for i in range(1,l+1):
 		tom.append(mot[l-i]) 
 	res = res.join(tom)
 	return res
+
 renv = renverse("informatique")
 print(renv)
