12f91fc2a19add85765490638b90fe049332cfe4
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-05-19 08:14:13 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+from func import *
+from mypackage.ConversionList import bin2dec
+n = '100101'
+d = bin2dec(n)
+print(n,'[base 2]','=',d,'[base 10]')
---
+++ mypackage/ConversionList.py
@@ -0,0 +1,12 @@
+def bin2dec(v):
+	"""Convert the given binary number to decimal number.
+
+    :param int text: word to reverse.
+
+    :returns: the reversed word .
+    """
+	return int(v, 2)
+def hexa2dec(v):
+	return int(v, 16)
+def dec2hexa(v):
+	return hex(65535)
