22eb17f6b1450093ff95be0b93a8d23a4f9fbb7a
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-05-24 06:47:55 +0100

    Add files via upload

---
+++ deriv.py
@@ -0,0 +1,9 @@
+# : Program that calculates the derivative of a functionfrom sympy import *
+import numpy as np
+
+x = Symbol('x')
+y = x**2
+# the derivative of a function :
+import numpy as np
+yp = y.diff(x)
+print(yp)
