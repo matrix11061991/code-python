299a926fbd23e58dba5ff3e519b9892c9eb2a75f
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-10-21 10:30:42 +0300

    Update derivation.py

--- derivation.py
+++ derivation.py
@@ -1,8 +1,9 @@
 # : function that calculates derivate of functions
 from sympy import symbols, cos, diff
+def calcDeriv(a, b, c)
+  a, b, c = symbols('a b c', real=True)
+  f = a**b - a**c + c**b
+  # differntiating function f in respect to a
+  return diff(f, a)
 
-a, b, c = symbols('a b c', real=True)
-f = a**b - a**c + c**b
 
-# differntiating function f in respect to a
-print(diff(f, a))
