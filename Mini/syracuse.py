611c73dd71d3e5fd719ddaf9ce18d016192d516e
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2023-01-02 11:13:45 +0200

    Update syracuse.py

--- Mini/syracuse.py
+++ Mini/syracuse.py
@@ -7,5 +7,12 @@ def syracuse(n: int) -> int:
             n = 3 * n + 1
         count += 1
     return count
-
+def syracuse2(n: int) -> int:
+    for i in range(n):
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        if n == 1:
+            return i + 1
 
